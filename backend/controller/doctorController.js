42a9dad8c580d363dd5644d8fdbaf652d8dd67ae
prasithdevanan <prasith4545@gmail.com>
2026-05-04 18:28:01 +0530

    Update patient

--- backend/controller/doctorController.js
+++ backend/controller/doctorController.js
@@ -3,12 +3,23 @@ import doctorModel from "../models/doctorModel.js";
 const getDoctors = async (req, res) => {
 
     try {
-        const doctorsList = await doctorModel.find();
+        const { search } = req.query;
+        let filter = {};
+        if (search) {
+            filter = {
+                $or:  [
+                    { name: { $regex: search, $options: "i" } },
+                    { email: { $regex: search, $options: "i" } },
+                ],
+            }
+        }
+        console.log(search)
+        const doctorsList = await doctorModel.find(filter);
         res.status(200).json({ success: true, doctorsList });
 
     } catch (error) {
         res.status(500).json({ success: false, message: error });
-     
+
     }
 }
 
